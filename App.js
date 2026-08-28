183c8df605bd92d7c529da342538a6ca55480fac
gianren17solana <rengian17solana@gmail.com>
2026-08-28 23:36:05 +0800

    Lab 3: Welcome screen with Flexbox layout

--- App.js
+++ App.js
@@ -1,17 +1,6 @@
-import { StyleSheet, View, Text } from "react-native";
-    import TaskCard from "./components/TaskCard";
+import WelcomeScreen from './screens/WelcomeScreen';
 
-          export default function App() {
-              return (
-               <View style={StyleSheet.container}>
-            <Text style={styles.heading}>My Tasks</Text>
-            <TaskCard title="Finish Lab 2" done= {false} /> 
-            <TaskCard title="Walk the dog" done={false} />     
-               </View> 
-              );
-            }
-            const styles = StyleSheet.create({
-           container: { flex: 1, paddingTop: 60, paddingHorizontal: 16, backgroundColor:
-            '#FFFFFF' },
-          heading: { fontSize: 24, fontWeight: 'bold', marginBottom: 12 },     
-            });
+export default function App() {
+return <WelcomeScreen />;
+
+}
